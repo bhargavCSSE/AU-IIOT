e40ddee8fb4b09c71ae169eb5d689e663a789644
jda0012 <54557625+jda0012@users.noreply.github.com>
2019-10-25 11:53:09 -0500

    Create dan_serial.py
    Start of breaking up the serial input as different elements. Might look more at the arduino code to break up the serial outputs as lines, and then be able to read the elements easier possibly.

---
+++ python_rsc/dan_serial.py
@@ -0,0 +1,8 @@
+import serial
+import time
+ser=serial>Serial('/dev/ttyUSB0')
+position=ser.read(100).decode('utf-8').split(";")
+xlist = [i for i in position if i.startswith("X")]
+ylist = [i for i in position if i.startswith("Y")]
+zlist = [i for i in position if i.startswith("Z")]
+tlist = [i for i in position if i.startswith("T")]
