a31b81d2521bddce7a9833afb2a547b145c13354
jda0012 <54557625+jda0012@users.noreply.github.com>
2019-10-28 11:58:09 -0500

    Update dan_serial.py

--- python_rsc/dan_serial.py
+++ python_rsc/dan_serial.py
@@ -1,6 +1,6 @@
 import serial
 import time
-ser=serial>Serial('/dev/ttyUSB0')
+ser=serial.Serial('/dev/ttyUSB0')
 position=ser.read(100).decode('utf-8').split(";")
 xlist = [i for i in position if i.startswith("X")]
 ylist = [i for i in position if i.startswith("Y")]
