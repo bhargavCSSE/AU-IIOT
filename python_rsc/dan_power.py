afa4b0d5bd72f7c2930074cb539ce2ef7e481168
jda0012 <54557625+jda0012@users.noreply.github.com>
2019-10-28 14:17:35 -0500

    Update dan_power.py

--- python_rsc/dan_power.py
+++ python_rsc/dan_power.py
@@ -3,8 +3,7 @@ import time
 r = redis.Redis(host='localhost', port = '6379', decode_responses=True, db=0)
 while True:
      basedata = r.get("basedata")
-     results = [float(i) for i in basedata.split(",")]
-     power1, power2, p1pp2, vrms, _,_,_,_,_,_,_ = results
+     power1, power2, p1pp2, vrms, _,_,_,_,_,_,_ = [float(i) for i in basedata.split(",")]
      print(power1)
      print(power2)
      time.sleep(1)
