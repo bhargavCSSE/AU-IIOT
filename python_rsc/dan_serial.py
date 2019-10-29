58ff3bfbc4d5e36bbb338c6b7b47219aef1a0b0f
jda0012 <54557625+jda0012@users.noreply.github.com>
2019-10-29 18:14:47 -0500

    Update dan_serial.py
    Avoids the Unicode Error. Able to take the long strings and just get the values I want.

--- python_rsc/dan_serial.py
+++ python_rsc/dan_serial.py
@@ -1,11 +1,18 @@
 import serial
 import time
-ser=serial.Serial('/dev/ttyUSB0')
-position=ser.readline().decode('utf-8').split(";")
-length=len(position)
-X,Y,Z,T,_=position[(length-5):]
-X,Y,Z,T=X[:1],Y[1:],Z[1:],T[1:]
-#xlist = [i for i in position if i.startswith("X")]
-#ylist = [i for i in position if i.startswith("Y")]
-#zlist = [i for i in position if i.startswith("Z")]
-#tlist = [i for i in position if i.startswith("T")]
+ser = serial.Serial(port='/dev/ttyUSB0',baudrate=9600,timeout=15)
+#time.sleep(5)
+while True:
+        try:
+                position=ser.readline().decode(encoding='utf-8',errors='ignore').split(";")
+                X=Y=Z=T=""
+                if len(position)<5:
+                        pass
+                elif len(position) == 5:
+                        X,Y,Z,T,_=position
+                else:
+                        X,Y,Z,T,_=position[(len(position)-5):]
+                line='{0},{1},{2},{3}'.format(X,Y,Z,T)
+                print(line)
+        except:
+                raise
