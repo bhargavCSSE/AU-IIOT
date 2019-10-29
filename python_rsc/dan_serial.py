285f1557d99d04617b3bd7fe726433ebf8afc9b3
jda0012 <54557625+jda0012@users.noreply.github.com>
2019-10-29 16:34:30 -0500

    Update dan_serial.py
    Add a printline function to the tachometer so it only sends measurements when the speed is there. Then dump the rest of the values and use only the last ones.

--- python_rsc/dan_serial.py
+++ python_rsc/dan_serial.py
@@ -1,8 +1,11 @@
 import serial
 import time
 ser=serial.Serial('/dev/ttyUSB0')
-position=ser.read(100).decode('utf-8').split(";")
-xlist = [i for i in position if i.startswith("X")]
-ylist = [i for i in position if i.startswith("Y")]
-zlist = [i for i in position if i.startswith("Z")]
-tlist = [i for i in position if i.startswith("T")]
+position=ser.readline().decode('utf-8').split(";")
+length=len(position)
+X,Y,Z,T,_=position[(length-5):]
+X,Y,Z,T=X[:1],Y[1:],Z[1:],T[1:]
+#xlist = [i for i in position if i.startswith("X")]
+#ylist = [i for i in position if i.startswith("Y")]
+#zlist = [i for i in position if i.startswith("Z")]
+#tlist = [i for i in position if i.startswith("T")]
