68ab4b9e6cde947b1d2aea30d91919b7232d121a
Bhargav Joshi <bhargav.csse@gmail.com>
2020-01-17 10:06:36 -0600

    Update on interrupt

--- python_rsc/interrupt-rpm-read.py
+++ python_rsc/interrupt-rpm-read.py
@@ -15,30 +15,53 @@ GPIO.setup(11,GPIO.IN)
 def interrupt_1(channel):
     global count
     count = count+1
-    print(count)
 
 GPIO.add_event_detect(11,GPIO.RISING,callback=interrupt_1)
 
-def counter():
+def thread_counter():
     while(var1 == 1):
         stamp1=count
-        sleep(60)
+        sleep(1)
         stamp2=count
         global ppm
-        ppm = (abs(stamp1 - stamp2))
+        ppm = 60*(abs(stamp1 - stamp2))
         # count = 0
-        print("Pulse per min: "+ str(ppm))
+        print("Pulse per min: "+ str(ppm)) 
 
 def thread_2():
     loop = 1
     while(var1 == 1):
         print("Interrupt-1 loop count= "+str(loop))
         loop = loop+1
-        sleep(1)
+        sleep(1/10)
 
-w2 = threading.Thread(name='counter', target=counter)
+def thread_3():
+    loop = 1
+    while(var1 == 1):
+        print("Interrupt-1 loop count= "+str(loop))
+        loop = loop+1
+        sleep(1/100)
 
-w2.start()
+def thread_4():
+    loop = 1
+    while(var1 == 1):
+        print("Interrupt-1 loop count= "+str(loop))
+        loop = loop+1
+        sleep(1/1000)
+
+t1 = threading.Thread(name='counter', target=thread_counter)
+t2 = threading.Thread(name='thread-2', target=thread_2)
+t3 = threading.Thread(name='thread-2', target=thread_3)
+t4 = threading.Thread(name='thread-2', target=thread_4)
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
 
 print("started")
 
+
+# grey: Digital
+# Purple: Ground
+# Blue: Voltage
