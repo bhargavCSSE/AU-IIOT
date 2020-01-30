62a74a4671f015c6ae588e2e13ca20a769adac05
Bhargav Joshi <bhargav.csse@gmail.com>
2020-01-29 23:47:24 -0600

    Dataframe thread added

--- python_rsc/Master-Code.py
+++ python_rsc/Master-Code.py
@@ -11,9 +11,10 @@ import board
 import RPi.GPIO as GPIO
 import adafruit_adxl34x
 import adafruit_ads1x15.ads1115 as ADS
+import pandas as pd
 
 from time import sleep
-from threading import Thread
+from threading import Thread, RLock
 from adafruit_ads1x15.analog_in import AnalogIn
 
 # Global Definitions
@@ -29,6 +30,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Kernel Setup
 count = 0
 loop_enable = 1
+lock = RLock()
 
 
 # GPIO setup
@@ -50,7 +52,7 @@ def thread_RPM():
     global count
     while(loop_enable == 1):
         stamp1=count
-        sleep(1)
+        sleep(0.5)
         stamp2=count
         global ppm
         ppm = 60*(abs(stamp1 - stamp2))
@@ -59,6 +61,8 @@ def thread_RPM():
 def thread_ADXL345():
     accelerometer = adafruit_adxl34x.ADXL345(i2c)
     while(loop_enable == 1):
+        global adxl
+        adxl = accelerometer.acceleration
         print("%f %f %f" % accelerometer.acceleration)
         sleep(1)
 
@@ -72,27 +76,35 @@ def thread_ADS1x15():
         #     x.append(((chan.voltage**2)**.5)*60)
         #     sleep(0.01)
         # current = max(x)
+        global current
         current = ((chan.voltage**2)**.5)*60
         print("Peak Current: " + str(current))
         sleep(0.1)
 
-def thread_4(): 
-    loop = 1
-    while(loop_enable == 1):
-        print("Interrupt-1 loop count= "+str(loop))
-        loop = loop+1
-        sleep(1)
+def thread_DataFrame(): 
+    print("Initializing...")
+    sleep(1)
+    print("DataFrame in action")
+    d = []
+    global ppm, adxl, current
+    for i in range(100):
+        d.append({'RPM': ppm, 'ADXL': adxl, 'Current': current})
+        df = pd.DataFrame(d)
+        sleep(0.1)
+    print(df)
 
 
 # Kernel Execution
-t1 = threading.Thread(name='RPM', target=thread_RPM)
-t2 = threading.Thread(name='Accel', target=thread_ADXL345)
-t3 = threading.Thread(name='ADS', target=thread_ADS1x15)
-#t4 = threading.Thread(name='thread-2', target=thread_4)
+t1 = threading.Thread(name='RPM', target=thread_RPM, daemon=True)
+t2 = threading.Thread(name='Accel', target=thread_ADXL345, daemon=True)
+t3 = threading.Thread(name='ADS', target=thread_ADS1x15, daemon=True)
+t4 = threading.Thread(name='DataFrame', target=thread_DataFrame)
 
 t1.start()
 t2.start()
 t3.start()
-#t4.start()
+t4.start()
 
 print("started")
+t4.join()
+print("Finished")
